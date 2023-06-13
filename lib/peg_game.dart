2d5ec8f0602f144a66b92cb72ccab2f13e4de72c
scottt <tbadg@centripetalfusion.com>
2023-06-12 23:49:23 -0400

    Create empty class for game widget and call it from main

---
+++ lib/peg_game.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PegGame extends StatefulWidget {
+  const PegGame({super.key});
+
+  @override
+  State<PegGame> createState() => _PegGame();
+}
+
+class _PegGame extends State<PegGame> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
